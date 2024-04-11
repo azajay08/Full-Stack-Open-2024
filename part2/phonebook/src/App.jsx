8e5c097e668242ba51457a11f623eb51fa4a6b92
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2024-04-11 21:15:27 +0300

    done up to part 2.8, still need to do checks

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -7,7 +7,7 @@ const Button = (props) => {
 }
 
 const People = ({people}) => {
-	const peopleList = people.map(props => <Person key={props.name} name={props.name}/>)
+	const peopleList = people.map(props => <Person key={props.name} name={props.name} number={props.number}/>)
 	return (
 		<div>
 			{peopleList}
@@ -17,37 +17,47 @@ const People = ({people}) => {
 
 const Person = (props) => {
 	return (
-		<div>{props.name}</div>
+		<div>{props.name} {props.number}</div>
 	)
 }
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas' }
-	  ]) 
+	const [persons, setPersons] = useState([]) 
 	const [newName, setNewName] = useState('')
+	const [newNumber, setNewNumber] = useState('')
 
 	
 	const addName = (event) => {
 		event.preventDefault()
 		const newPerson = {
 			name: newName,
-			id: newName,
-			// number: newNumber,
+			// id: newName,
+			number: newNumber,
 		}
-		setPersons(persons.concat(newPerson))
-		setNewName('')
+
+		const nameExists = persons.find(props => props.name.toLowerCase() === newPerson.name.toLowerCase())
+		
+		if (nameExists) {
+			window.alert(`${newName} is already added to phonebook`)
+		}
+		else {
+			setPersons(persons.concat(newPerson))
+			setNewName('')
+			setNewNumber('')
+		}
 	}
 	
 	const handleAdd = (event) => { setNewName(event.target.value) }
+
+	const handleNumber = (event) => { setNewNumber(event.target.value)}
 
 	return (
 		<div>
 		<h2>Phonebook</h2>
 		<form onSubmit={addName}>
 			<div>
-				name: <input id="nameInput" value={newName}
-				onChange={handleAdd} />
+				<div>name: <input id="nameInput" value={newName} onChange={handleAdd}/></div>
+				<div>number: <input id='numberInput' value={newNumber} onChange={handleNumber}/></div>
 			</div>
 			<Button type="submit" text='add'/>
 		</form>
