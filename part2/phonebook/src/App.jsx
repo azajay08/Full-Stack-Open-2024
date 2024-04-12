1a805b6bc693c18489ee51f885c081ccfaf3e284
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2024-04-12 20:53:49 +0300

    finished up tp 2.10 but need to clean

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 
-const Button = (props) => {
+const Button = ({type, text}) => {
 	return (
-		<button type={props.type} >{props.text}</button>
+		<button type={type} >{text}</button>
 	)
 }
 
-const People = ({people}) => {
-	const peopleList = people.map(props => <Person key={props.name} name={props.name} number={props.number}/>)
+const Persons = ({people}) => {
+	const peopleList = people.map(props => <Person key={props.id} name={props.name} number={props.number}/>)
 	return (
 		<div>
 			{peopleList}
@@ -15,24 +15,50 @@ const People = ({people}) => {
 	)
 }
 
-const Person = (props) => {
+const Person = ({name, number}) => {
 	return (
-		<div>{props.name} {props.number}</div>
+		<div>{name} {number}</div>
+	)
+}
+
+const Header = ({text}) => <h2>{text}</h2>
+
+const Filter = ({text, value, handleChange}) => {
+	return (
+		<div>
+			{text} <input id="searchInput" value={value} onChange={handleChange}/>
+		</div>
+	)
+}
+
+const PersonForm = (props) => {
+	return (
+		<form onSubmit={props.onSubmit}>
+			<div>{props.name} <input id="nameInput" value={props.newName} onChange={props.handleNewName}/></div>
+			<div>{props.number} <input id="numberInput" value={props.newNumber} onChange={props.handleNewNumber}/></div>
+			<Button type="submit" text='add'/>
+		</form>
 	)
 }
 
 const App = () => {
-	const [persons, setPersons] = useState([]) 
+	const [persons, setPersons] = useState([
+		{ name: 'Arto Hellas', number: '040-123456', id: 1 },
+		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+		{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+	  ])
 	const [newName, setNewName] = useState('')
 	const [newNumber, setNewNumber] = useState('')
+	const [filterName, setFilterName] = useState('')
 
 	
-	const addName = (event) => {
+	const addContact = (event) => {
 		event.preventDefault()
 		const newPerson = {
 			name: newName,
-			// id: newName,
 			number: newNumber,
+			id: persons.length + 1,
 		}
 
 		const nameExists = persons.find(props => props.name.toLowerCase() === newPerson.name.toLowerCase())
@@ -47,22 +73,30 @@ const App = () => {
 		}
 	}
 	
-	const handleAdd = (event) => { setNewName(event.target.value) }
+	const filteredPersons = persons.map(person => person.name.toLowerCase().includes(filterName.toLowerCase())) ?
+	persons.filter(person => person.name.toLowerCase().includes(filterName.toLowerCase())) :
+	persons
 
-	const handleNumber = (event) => { setNewNumber(event.target.value)}
+	const handleNewName = (event) => { setNewName(event.target.value) }
+
+	const handleNewNumber = (event) => { setNewNumber(event.target.value)}
+
+	const handleFilter = (event) => { setFilterName(event.target.value) }
 
 	return (
 		<div>
-		<h2>Phonebook</h2>
-		<form onSubmit={addName}>
-			<div>
-				<div>name: <input id="nameInput" value={newName} onChange={handleAdd}/></div>
-				<div>number: <input id='numberInput' value={newNumber} onChange={handleNumber}/></div>
-			</div>
-			<Button type="submit" text='add'/>
-		</form>
-		<h2>Numbers</h2>
-		<People people={persons}/>
+		<Header text='Phonebook'/>
+		<Filter text='filter shown with' value ={filterName} handleChange={handleFilter} />
+		<Header text='Add a new'/>
+		<PersonForm onSubmit={addContact}
+			name={'name:'}
+			number={'number:'}
+			newName={newName}
+			newNumber={newNumber}
+			handleNewName={handleNewName}
+			handleNewNumber={handleNewNumber}/>
+		<Header text='Numbers'/>
+		<Persons people={filteredPersons}/>
 		</div>
 	)
 }
