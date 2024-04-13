0dfdf708e08aeaac4d95e1c485bee27de0679b3f
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2024-04-13 21:56:26 +0300

    2.11 done?

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect  } from 'react'
+import axios from 'axios'
 
 const Button = ({type, text}) => <button type={type}> {text}</button>
 
@@ -45,17 +46,19 @@ const PersonForm = ({onSubmit, newName, newNumber, handleNewName, handleNewNumbe
 }
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', number: '040-123456', id: 1 },
-		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-		{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-	  ])
+	const [persons, setPersons] = useState([])
 	const [newName, setNewName] = useState('')
 	const [newNumber, setNewNumber] = useState('')
 	const [filterName, setFilterName] = useState('')
 
-	
+	useEffect(() => {
+		axios
+		  .get('http://localhost:3001/persons')
+		  .then(response => {
+			setPersons(response.data)
+		  })
+	  }, [])
+
 	const addContact = (event) => {
 		event.preventDefault()
 		const newPerson = {
@@ -87,6 +90,7 @@ const App = () => {
 	const handleNewNumber = (event) => { setNewNumber(event.target.value)}
 
 	const handleFilter = (event) => { setFilterName(event.target.value) }
+	
 
 	return (
 		<div>
