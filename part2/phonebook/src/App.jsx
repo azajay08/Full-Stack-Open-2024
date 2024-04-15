349f80d0a843dc3589d561988fa215d9c210743b
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2024-04-15 19:00:47 +0300

    part2.14 done, need more checking

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,24 +1,17 @@
-import { useState, useEffect  } from 'react'
-import axios from 'axios'
-import personService from './services/persons'
-
-const Button = ({type, text}) => <button type={type}> {text}</button>
-
-const Persons = ({people}) => {
-	const peopleList = people.map
-	(props => <Person key={props.id} name={props.name} number={props.number}/>)
-	return (
-		<div>
-			{peopleList}
-		</div>
-	)
-}
-
-const Person = ({name, number}) => <div>{name} {number}</div>
+import { useState, useEffect } from 'react'
+import personServices from './services/persons'
 
 const Header = ({text}) => <h2>{text}</h2>
 
 const SubHeader = ({text}) => <h3>{text}</h3>
+
+const Persons = ({people}) => <div>{people}</div>
+
+const Button = ({type, text, handleChange}) => {
+	return(
+		<button type={type} onClick={handleChange}> {text}</button>
+	)
+}
 
 const Filter = ({text, value, handleChange}) => {
 	return (
@@ -30,7 +23,9 @@ const Filter = ({text, value, handleChange}) => {
 
 const FormBox = ({text, inputName, value, handleChange}) => {
 	return (
-		<div>{text} <input id={inputName} value={value} onChange={handleChange}/></div>
+		<div>
+			{text} <input id={inputName} value={value} onChange={handleChange}/>
+		</div>
 	)
 }
 
@@ -53,7 +48,7 @@ const App = () => {
 	const [filterName, setFilterName] = useState('')
 
 	useEffect(() => {
-		personService
+		personServices
 		  .getAll()
 		  .then(initialResponse => {
 			setPersons(initialResponse)
@@ -64,18 +59,18 @@ const App = () => {
 		event.preventDefault()
 		const newPerson = {
 			name: newName,
-			number: newNumber,
-			// id: persons.length + 1,
+			number: newNumber
 		}
 
 		const nameExists = persons.find
 		(props => props.name.toLowerCase() === newPerson.name.toLowerCase())
 		
+		console.log(nameExists)
 		if (nameExists) {
 			window.alert(`${newName} is already added to phonebook`)
 		}
 		else {
-			personService
+			personServices
 			.create(newPerson)
 			.then(returnedPerson => {
 				setPersons(persons.concat(returnedPerson))
@@ -84,18 +79,42 @@ const App = () => {
 			})
 		}
 	}
+
+	const deletePerson = (id) => {
+		const contact = persons.find(n => n.id === id)
+		if(window.confirm(`Delete ${contact.name} ?`))
+		{
+			personServices
+			.getDeletedPerson(id)
+			setPersons(persons.filter(contact => contact.id !== id))
+		}
+	}
 	
-	const filteredPersons = persons.map
-	(person => person.name.toLowerCase().includes(filterName.toLowerCase())) ?
-	persons.filter(person => person.name.toLowerCase().includes(filterName.toLowerCase())) :
-	persons
-
 	const handleNewName = (event) => { setNewName(event.target.value) }
-
+	
 	const handleNewNumber = (event) => { setNewNumber(event.target.value)}
-
+	
 	const handleFilter = (event) => { setFilterName(event.target.value) }
 	
+	
+	const Person = ({name, number, id}) => {
+		return (
+			<div>
+				{name} {number}
+				<Button type="submit" text='delete'
+				handleChange={() => deletePerson(id)} />
+			</div>
+		)
+	}
+
+	const filteredPersons = persons.map
+		(person => person.name.toLowerCase().includes(filterName.toLowerCase())) ?
+		persons.filter(person => person.name.toLowerCase().includes(filterName.toLowerCase())) :
+		persons
+
+	const people = filteredPersons.map(props =>
+		<Person key={props.id} name={props.name} number={props.number} id={props.id}/>)
+
 
 	return (
 		<div>
@@ -106,7 +125,7 @@ const App = () => {
 			newName={newName} newNumber={newNumber}
 			handleNewName={handleNewName} handleNewNumber={handleNewNumber}/>
 		<Header text='Numbers'/>
-		<Persons people={filteredPersons}/>
+		<Persons people={people}/>
 		</div>
 	)
 }
--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -17,6 +17,11 @@ const update = (id, newObject) => {
 	return request.then(response => response.data)
 }
 
+const getDeletedPerson = id => {
+	const request = axios.delete(`${baseUrl}/${id}`)
+	return request.then(response => response.data)
+}
+
 export default { 
-	getAll, create, update 
+	getAll, create, update, getDeletedPerson
 }
