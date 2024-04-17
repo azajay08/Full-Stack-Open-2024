63f220471bb3ebbe442924392f14d70d06142621
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2024-04-17 17:17:41 +0300

    data for countries done, just last minute checks

--- part2/dataforcountries/src/App.jsx
+++ part2/dataforcountries/src/App.jsx
@@ -1,9 +1,28 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const CountryPreview = ({country}) => {
-	return (
-		<div>{country.name.common}</div>
+const Language = ({language}) => <li>{language}</li>
+
+const Weather = ({capital}) => {
+	const[forecast, setForecast] = useState(null)
+	const api_key = import.meta.env.VITE_API_KEY
+
+	useEffect(() => {
+		axios
+			.get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}&units=metric`)
+			.then(response => {
+				console.log(response.data)
+				setForecast(response.data)
+			})
+	}, [])
+	
+	return ( forecast === null ? null :
+		<div>
+			<h2>Weather in {capital}</h2>
+			<div>{`tempertaure ${(forecast.main.temp).toFixed(2)} Celcius`}</div>
+			<img alt="weather icon" src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`} />
+			<div>{`wind ${forecast.wind.speed} m/s`}</div>
+		</div>
 	)
 }
 
@@ -16,17 +35,37 @@ const Country = ({country}) => {
 			<div>area: {country.area}</div>
 			<h3>languages:</h3>
 			<ul>
-				{languages.map(language => 
-				<li key={language}> {country.languages[language]}</li>)}
+				{languages.map(language =>
+				<Language key={language} language={country.languages[language]}/>)}
 			</ul>
 			<img src={country.flags.png} alt='flag' height='200' width='250' />
+			<Weather capital={country.capital}/>
+
 		</div>
 	)
 }
 
-const FilterPreview = ({result}) => {
+const CountryPreview = ({country}) => {
+	const [showButton, setShowButton] = useState(false)
+
+	const handleClick= () => {
+		setShowButton(!showButton)
+	}
+
+	return (
+		<div>
+			{country.name.common} <button onClick={handleClick}>{showButton === false ? 'show': 'hide'}</button>
+			{showButton === true && <Country key={country.name.common} country={country} />}	
+		</div>
+	)
 	
-	if (result.length > 10) {
+} 
+
+const Filter = ({result, searchInput}) => {
+	if (!searchInput) {
+		return
+	}
+	if (searchInput && result.length > 10) {
 		return (
 			'Too many matches, specify another filter'
 		)
@@ -62,10 +101,11 @@ const App = () => {
 	return (
 		<div>
 			<form>
-				find countries <input value={country} onChange={handleCountryChange}/>
+				find countries <input id="SearchInput" value={country} onChange={handleCountryChange}/>
 			</form>
 			<div>
-				<FilterPreview key={filteredCountries.id} result={filteredCountries} country={country} />
+				<Filter key={filteredCountries.id} result={filteredCountries}
+				 searchInput={country.length > 0 ? true : false} />
 			</div>
 		</div>
 	)
