eac25bbfaeb5fa86a620306dcb0d0b5c881fc366
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2024-05-07 20:59:02 +0300

    added test helper and started test

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,25 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+
+const helper = require('./test_helper')
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promsieArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promsieArray) 
+})
+
+
+
+after(async () => {
+  await mongoose.connection.close()
+})
