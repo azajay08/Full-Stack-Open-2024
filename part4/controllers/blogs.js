6226df4d4271e7f605e89f01a7f6063f9a36b4e3
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2024-05-11 18:20:21 +0300

    part 4.10 done

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,35 +1,33 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const { blogsInDb } = require('../tests/test_helper')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.get('/:id', (request, response) => {
-  Blog.
-    findById(request.params.id)
-    .then(blog => {
-      if (blog) {
-        return (response.json(blog))
-      }
-      else {
-        response.status(404).end()
-      }
-    })
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    like: body.likes,
+  })
+
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
 module.exports = blogsRouter
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -12,10 +12,7 @@ const Blog = require('../models/blog')
 beforeEach(async () => {
   await Blog.deleteMany({})
 
-  const blogObjects = helper.initialBlogs
-    .map(blog => new Blog(blog))
-  const promsieArray = blogObjects.map(blog => blog.save())
-  await Promise.all(promsieArray) 
+  await Blog.insertMany(helper.initialBlogs) 
 })
 
 test('blogs are returned as json', async () => {
@@ -29,6 +26,38 @@ test('correct amount of blogs are returned', async () => {
   const respone = await api.get('/api/blogs')
 
   assert.strictEqual(respone.body.length, helper.initialBlogs.length)
+})
+
+test('unique identifier property of the blog posts is named id', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+
+  const blogToView = blogsAtStart[0]
+
+  resultBlog = await api
+    .get(`/api/blogs/${blogToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.deepStrictEqual(resultBlog.body, blogToView)
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Blogger',
+    author: 'Joe Blogs',
+    url: 'www.averagejoe.com',
+    likes: 2,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
 })
 
 after(async () => {
