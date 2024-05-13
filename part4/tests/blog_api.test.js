ed6a99533938680fa153af577ef1c5fd0e9fde3f
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2024-05-13 19:06:25 +0300

    refactoring tests

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -15,111 +15,118 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs) 
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('when there is initially some blogs saved', () => {
+
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+  
+  test('correct amount of blogs are returned', async () => {
+    const respone = await api.get('/api/blogs')
+    
+    assert.strictEqual(respone.body.length, helper.initialBlogs.length)
+  })
+  
+  test('unique identifier property of the blog posts is named id', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    
+    const blogToView = blogsAtStart[0]
+    
+    resultBlog = await api
+      .get(`/api/blogs/${blogToView.id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    
+    assert.deepStrictEqual(resultBlog.body, blogToView)
+  })
+  
+  describe('addition of new note', () => {
+
+    
+    test('a valid blog can be added', async () => {
+      const newBlog = {
+        title: 'Blogger',
+        author: 'Joe Blogs',
+        url: 'www.averagejoe.com',
+        likes: 2,
+      }
+      
+      await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+      
+      const blogsAtEnd = await helper.blogsInDb()
+      
+      const contents = blogsAtEnd.map(blog => blog.title)
+      
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+      
+      assert(contents.includes('Blogger'))
+    })
+    
+    test('a new blog missing likes property defaults to 0', async () => {
+      const newBlog = {
+        title: 'Blogger',
+        author: 'Joe Blogs',
+        url: 'www.averagejoe.com',
+      }
+      
+      await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+      
+      const blogsAtEnd = await helper.blogsInDb()
+      
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+      
+      const blogToView = blogsAtEnd[blogsAtEnd.length - 1]
+      
+      assert.strictEqual(blogToView.likes, 0)
+    })
+    
+    test('a new blog missing title property will not be added', async () => {
+      const newBlog = {
+        author: 'Joe Blogs',
+        url: 'www.averagejoe.com',
+        likes: 3,
+      }
+      
+      await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+      
+      const blogsAtEnd = await helper.blogsInDb()
+      
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+    })
+    
+    test('a new blog missing url property will not be added', async () => {
+      const newBlog = {
+        title: 'Blogger',
+        author: 'Joe Blogs',
+        likes: 3,
+      }
+      
+      await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+      
+      const blogsAtEnd = await helper.blogsInDb()
+      
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+    })
+  })
 })
-
-test('correct amount of blogs are returned', async () => {
-  const respone = await api.get('/api/blogs')
-
-  assert.strictEqual(respone.body.length, helper.initialBlogs.length)
-})
-
-test('unique identifier property of the blog posts is named id', async () => {
-  const blogsAtStart = await helper.blogsInDb()
-
-  const blogToView = blogsAtStart[0]
-
-  resultBlog = await api
-    .get(`/api/blogs/${blogToView.id}`)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  assert.deepStrictEqual(resultBlog.body, blogToView)
-})
-
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'Blogger',
-    author: 'Joe Blogs',
-    url: 'www.averagejoe.com',
-    likes: 2,
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-
-  const contents = blogsAtEnd.map(blog => blog.title)
-
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
-
-  assert(contents.includes('Blogger'))
-})
-
-test('a new blog missing likes property defaults to 0', async () => {
-  const newBlog = {
-    title: 'Blogger',
-    author: 'Joe Blogs',
-    url: 'www.averagejoe.com',
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
-
-  const blogToView = blogsAtEnd[blogsAtEnd.length - 1]
-
-  assert.strictEqual(blogToView.likes, 0)
-})
-
-test('a new blog missing title property will not be added', async () => {
-  const newBlog = {
-    author: 'Joe Blogs',
-    url: 'www.averagejoe.com',
-    likes: 3,
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  const blogsAtEnd = await helper.blogsInDb()
-
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
-})
-
-test('a new blog missing url property will not be added', async () => {
-  const newBlog = {
-    title: 'Blogger',
-    author: 'Joe Blogs',
-    likes: 3,
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  const blogsAtEnd = await helper.blogsInDb()
-
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
-})
-
+  
 after(async () => {
   await mongoose.connection.close()
 })
