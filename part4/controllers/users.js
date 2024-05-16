28bcfd63ab97c6568d20d3d00f28e6511768c006
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2024-05-16 17:35:21 +0300

    part 4.15 done

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -10,18 +10,26 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
-
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  })
-
-  const savedUser = await user.save()
-
-  response.status(201).json(savedUser)
+  if (username === undefined || password === undefined) {
+    return response.status(400).json({ error: 'username and password must be given'})
+  } else if (username.length < 3) {
+    return response.status(400).json({ error: 'username must be at least 3 characters long'})
+  } else if (password.length < 3) {
+    return response.status(400).json({ error: 'password must be at least 3 characters long'})
+  } else {
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+  
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    })
+  
+    const savedUser = await user.save()
+  
+    response.status(201).json(savedUser)
+  }
 })
 
 module.exports = usersRouter
