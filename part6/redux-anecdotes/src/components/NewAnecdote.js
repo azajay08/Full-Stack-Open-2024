eccbe2a21e1e473ee426cfee5b470e1ef6f241b4
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2024-07-04 12:26:22 +0300

    starting 6.4

---
+++ part6/redux-anecdotes/src/components/NewAnecdote.js
@@ -0,0 +1,22 @@
+import { useDispatch } from 'react-redux'
+import { createAnecodote } from '../reducers/anecdoteReducer'
+
+const NewAnecdote = (props) => {
+  const dispatch = useDispatch()
+
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    dispatch(createAnecodote(content))
+  }
+
+  return (
+    <form onSubmit={addAnecdote}>
+      <input name="anecdote" />
+      <button type="submit">add</button>
+    </form>
+  )
+}
+
+export default NewNote
