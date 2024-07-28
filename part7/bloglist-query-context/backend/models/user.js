0979daa96152622ae0c7ae2fee8cedaa5796b3b8
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2024-07-28 13:53:21 +0300

    added prettier, need to implement into the editor

--- part7/bloglist-query-context/backend/models/user.js
+++ part7/bloglist-query-context/backend/models/user.js
@@ -1,31 +1,30 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   blogs: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Blog'
-    }
+      ref: "Blog",
+    },
   ],
   username: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   name: String,
   passwordHash: String,
-})
+});
 
-
-userSchema.set('toJSON', {
+userSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-    delete returnedObject.passwordHash
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash;
+  },
+});
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
