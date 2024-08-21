1020bc7cd45af61cf6494001278021fe9904ad36
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2024-08-21 16:29:25 +0300

    starting redux 7.10

--- part7/bloglist-query-context/frontend/src/components/Notification.jsx
+++
@@ -1,9 +0,0 @@
-const Notification = ({ message, errorStatus }) => {
-  if (message === null) {
-    return null;
-  }
-
-  return <div className={errorStatus ? "error" : "success"}>{message}</div>;
-};
-
-export default Notification;
